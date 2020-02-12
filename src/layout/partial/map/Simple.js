beacfc7c4b9b33a07be2057abbe44e737633d88b
王澍(Andrew) <andrewwang1@micb2b.com>
2020-02-12 13:38:11 +0800

    remove google api key

--- src/layout/partial/map/Simple.js
+++ src/layout/partial/map/Simple.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import GoogleMapReact from 'google-map-react';
 import Styles from './Map.module.css';
+import Configs from 'configs/googleMapConfig';
 
 const AnyReactComponent = ({text}) => (
     <div className={Styles.redCircle}>
@@ -21,7 +22,7 @@ class SimpleMap extends Component {
         return (
             // Important! Always set the container height explicitly
             <GoogleMapReact
-                bootstrapURLKeys={{key: 'AIzaSyDFP0Eo3_s2rK0o-HsnDo1xgYg4nvb7B_E'}}
+                bootstrapURLKeys={{key: Configs.key}}
                 defaultCenter={this.props.center}
                 defaultZoom={this.props.zoom}
             >
