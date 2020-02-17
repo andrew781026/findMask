77dfc9585008d79b0f3aeca8fdf372508fd0db3a
王澍(Andrew) <andrewwang1@micb2b.com>
2020-02-17 12:30:04 +0800

    刪除 身分證 , 輔助文字

--- src/layout/partial/toolbar/Toolbar.js
+++ src/layout/partial/toolbar/Toolbar.js
@@ -40,30 +40,6 @@ const Toolbar = (props) => (
             <div className='text-xl my-4'>02/10(一)</div>
             <div className='font-500' style={{fontSize: '12px'}}>如今日購買，下次最快可購買日為02/17(一)</div>
         </div>
-        <div className={Styles.center_content}>
-
-            <div className='flex justify-end items-center text-white my-4 font-500' style={{fontSize: '10px'}}>
-                <img src={settingPng} alt='設定身分證尾碼'/>
-                設定身分證尾碼
-            </div>
-
-            <div className='relative'>
-                <div className='flex justify-center items-center text-white bg-grey-darkest'>
-                    {getText(8)}
-                </div>
-
-                {false && (
-                    <div className={Styles.tooltip}>
-                        <div className={Styles.tooltip_span}>請輸入您的身份證字號尾碼</div>
-                        <div>
-                            <input type="text" className={Styles.input}/>
-                            <button className={Styles.tooltip_button}>儲存</button>
-                        </div>
-                    </div>
-                )}
-            </div>
-
-        </div>
         <div className={Styles.right_content}>
             <div className='flex-col flex-1 text-white mt-4 mx-8'>
                 <p className={Styles.right_p}>民眾持本人 「健保卡」 ，每人現購 2 片，7 天內不可重複購買 </p>
