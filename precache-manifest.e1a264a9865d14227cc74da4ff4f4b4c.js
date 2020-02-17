db0ed130b4f82f900e20ed2859f9ddab53071bda
王澍(Andrew) <andrewwang1@micb2b.com>
2020-02-17 13:20:17 +0800

    Updates

---
+++ precache-manifest.e1a264a9865d14227cc74da4ff4f4b4c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913c1bd2aac98a3e99d3aba855871ec0",
+    "url": "/findMask/index.html"
+  },
+  {
+    "revision": "1e0e02edede765ac0ea6",
+    "url": "/findMask/static/css/2.7f75bbe2.chunk.css"
+  },
+  {
+    "revision": "e7c21406ddb177cde1e4",
+    "url": "/findMask/static/css/main.b5312f2a.chunk.css"
+  },
+  {
+    "revision": "1e0e02edede765ac0ea6",
+    "url": "/findMask/static/js/2.bdd3cd01.chunk.js"
+  },
+  {
+    "revision": "ed0e5755db3633738d1f774b733029b7",
+    "url": "/findMask/static/js/2.bdd3cd01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7c21406ddb177cde1e4",
+    "url": "/findMask/static/js/main.16ceddbb.chunk.js"
+  },
+  {
+    "revision": "6b15fb0d592d220643e3",
+    "url": "/findMask/static/js/runtime-main.a289539a.js"
+  },
+  {
+    "revision": "e91002739bb57a0b595cd1c4faa49935",
+    "url": "/findMask/static/media/adult-face.e9100273.svg"
+  },
+  {
+    "revision": "4856657c04170ee05335a12d907c1140",
+    "url": "/findMask/static/media/child-face.4856657c.svg"
+  },
+  {
+    "revision": "54749a5adbc8761267a12d9db0f4726e",
+    "url": "/findMask/static/media/clock-regular.54749a5a.svg"
+  },
+  {
+    "revision": "584f89de21cedb71ab251833b7a733ca",
+    "url": "/findMask/static/media/phone-solid.584f89de.svg"
+  },
+  {
+    "revision": "578398cee8105e54986283266ceee98a",
+    "url": "/findMask/static/media/phone.578398ce.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3cc354cabd9bce634e728a21b3556aec.js"
+  "/findMask/precache-manifest.e1a264a9865d14227cc74da4ff4f4b4c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/findMask/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
