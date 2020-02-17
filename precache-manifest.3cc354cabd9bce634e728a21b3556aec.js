7e74d8047cc8a21c4be2e46b5c91d4f5157e5567
王澍(Andrew) <andrewwang1@micb2b.com>
2020-02-17 13:11:06 +0800

    Updates

---
+++ precache-manifest.3cc354cabd9bce634e728a21b3556aec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58764aae20c830eec963ab1df83962e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e0e02edede765ac0ea6",
+    "url": "/static/css/2.7f75bbe2.chunk.css"
+  },
+  {
+    "revision": "a6d39b9d5204d967a901",
+    "url": "/static/css/main.b5312f2a.chunk.css"
+  },
+  {
+    "revision": "1e0e02edede765ac0ea6",
+    "url": "/static/js/2.bdd3cd01.chunk.js"
+  },
+  {
+    "revision": "ed0e5755db3633738d1f774b733029b7",
+    "url": "/static/js/2.bdd3cd01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d39b9d5204d967a901",
+    "url": "/static/js/main.47ed9110.chunk.js"
+  },
+  {
+    "revision": "47bd49c57fde4dfe9acc",
+    "url": "/static/js/runtime-main.5a6915b4.js"
+  },
+  {
+    "revision": "e91002739bb57a0b595cd1c4faa49935",
+    "url": "/static/media/adult-face.e9100273.svg"
+  },
+  {
+    "revision": "4856657c04170ee05335a12d907c1140",
+    "url": "/static/media/child-face.4856657c.svg"
+  },
+  {
+    "revision": "54749a5adbc8761267a12d9db0f4726e",
+    "url": "/static/media/clock-regular.54749a5a.svg"
+  },
+  {
+    "revision": "584f89de21cedb71ab251833b7a733ca",
+    "url": "/static/media/phone-solid.584f89de.svg"
+  },
+  {
+    "revision": "578398cee8105e54986283266ceee98a",
+    "url": "/static/media/phone.578398ce.svg"
+  }
+]);
